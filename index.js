f8ab0f59b691731757342f0d14324954446ea37f
Aayush Raj Joshi <TheARJ>
2021-12-06 12:52:42 +0545

    initial Commit

---
+++ index.js
@@ -0,0 +1,25 @@
+// 1.Ways to print in java script
+
+//  console.log("Hello World");
+//  alert("Alert Me")
+// document.write("Thus is Document Write")
+//--------------------------------------------------
+// 2.Java Script Console API
+
+// console.log("Hello World", 4 + 6, "Another Log");
+// console.warn("This is warning");
+// console.error("This is Error");
+
+// --------------------------------------------------
+
+// 3. JavaScript Variables
+
+// What are variables? Containers are to store data valules
+var number1 = 34;
+var number2 = 56;
+console.log(number1+number2)
+
+// 4. Data Types in Data Types
+
+
+
